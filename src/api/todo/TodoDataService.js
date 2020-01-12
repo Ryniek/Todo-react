b32b0bd748e1ca85efc68a0ad00ebc9973837afb
Michal Rynski <michalrynski96@gmail.com>
2020-01-13 00:25:35 +0100

    final commit

--- src/api/todo/TodoDataService.js
+++ src/api/todo/TodoDataService.js
@@ -3,23 +3,23 @@ import axios from 'axios'
 class TodoDataService {
 
     retrieveAllTodos = (name) => {
-        return axios.get(`http://michu-api.herokuapp.com/users/${name}/todos`);
+        return axios.get(`https://michu-api.herokuapp.com/users/${name}/todos`);
     }
 
     deleteTodo = (name, id) => {
-        return axios.delete(`http://michu-api.herokuapp.com/users/${name}/todos/${id}`)
+        return axios.delete(`https://michu-api.herokuapp.com/users/${name}/todos/${id}`)
     }
 
     retrieveTodo = (name, id) => {
-        return axios.get(`http://michu-api.herokuapp.com/users/${name}/todos/${id}`)
+        return axios.get(`https://michu-api.herokuapp.com/users/${name}/todos/${id}`)
     }
 
     updateTodo = (name, id, todo) => {
-        return axios.put(`http://michu-api.herokuapp.com/users/${name}/todos/${id}`, todo)
+        return axios.put(`https://michu-api.herokuapp.com/users/${name}/todos/${id}`, todo)
     }
 
     saveTodo = (name, todo) => {
-        return axios.post(`http://michu-api.herokuapp.com/users/${name}/todos`, todo)
+        return axios.post(`https://michu-api.herokuapp.com/users/${name}/todos`, todo)
     }
 }
 
